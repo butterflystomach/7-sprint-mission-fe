cf4887997dbd63b17a73174941d24c67c5fc1d48
KimJesung <andrewkim99@naver.com>
2025-05-31 14:52:18 +0900

    feat: 파일 추가 및 수정

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,16 @@
 import Header from './Header.jsx';
+import Section from './Section.jsx';
+import Footer from './Footer.jsx';
+import './section.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Section />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
