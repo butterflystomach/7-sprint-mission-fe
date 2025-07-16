34a50ae4b9572f214f2d20558f4cc0c1d5abb4a4
KimJesung <andrewkim99@naver.com>
2025-07-16 14:25:34 +0900

    docs: pages 폴더 내 파일 작성 및 layout 폴더 생성

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
-import Container from "./Container.js";
+import Container from "../Container.js";
 
 export default function Header() {
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import Layout from "@/components/layout/Layout";
+import GlobalStyle from "@/styles/GlobalStyle";
+import theme from "@/styles/theme";
+import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
