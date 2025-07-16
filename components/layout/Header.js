e97e6d5f939a47d65b0673c63b8b22e294f28ca0
KimJesung <andrewkim99@naver.com>
2025-07-16 16:15:06 +0900

    feat: 아이콘 이미지 추가 및 components 파일 수정

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,24 +1,87 @@
+import Image from "next/image";
 import Link from "next/link";
-import styles from "./Header.module.css";
-import Container from "../Container.js";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+import { StyledLink } from "@/styles/CommonStyles";
 
-export default function Header() {
-  return (
-    <header className={styles.header}>
-      <Container className={styles.container}>
-        <Link className={styles.logo} href="/">
-          판다마켓
-        </Link>
-        <Link className={styles.setting} href="/setting">
-          자유게시판
-        </Link>
-        <Link className={styles.setting} href="/setting">
-          중고마켓
-        </Link>
-        <Link className={styles.button} href="/setting">
-          로그인
-        </Link>
-      </Container>
-    </header>
-  );
+const GlobalHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderLogo = styled(Link)`
+  margin-right: 16px;
+
+  @media (min-width: 768px) {
+    margin-right: 35px;
+  }
+
+  @media (min-width: 1280px) {
+    margin-right: 47px;
+  }
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 8px;
+  font-weight: bold;
+  font-size: 16px;
+  color: var(--gray-600);
+
+  @media (min-width: 768px) {
+    gap: 36px;
+    font-size: 18px;
+  }
+`;
+
+const NavItem = styled.li`
+  a:hover {
+    color: var(--blue);
+  }
+`;
+
+const LoginLink = styled(StyledLink)``;
+
+function getLinkStyle(isActive) {
+  return { color: isActive ? "var(--blue)" : undefined };
 }
+
+const Header = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <GlobalHeader>
+      <HeaderLeft>
+        <HeaderLogo href="/" aria-label="홈으로 이동">
+          <Image
+            src="/images/logo.svg"
+            alt="판다마켓 로고"
+            width={153}
+            height={40}
+          />
+        </HeaderLogo>
+
+        <nav>
+          <NavList>
+            <NavItem>
+              <Link href="/board" style={getLinkStyle(pathname === "/board")}>
+                자유게시판
+              </Link>
+            </NavItem>
+          </NavList>
+        </nav>
+      </HeaderLeft>
+
+      <LoginLink href="/login">로그인</LoginLink>
+    </GlobalHeader>
+  );
+};
+
+export default Header;
